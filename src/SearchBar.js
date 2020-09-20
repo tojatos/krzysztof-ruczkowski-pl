9d0857eae27455fc9c2d8925bacdd9d6c6371059
tojatos <tojatos@gmail.com>
2020-09-20 16:12:54 +0200

    Improved search bar and portfolio on mobile

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -4,7 +4,7 @@ import './SearchBar.sass'
 const SearchBar = ({placeholderText, onChange}) => {
     return (
         <div className="search-bar">
-            <input type="text" placeholder={placeholderText} onChange={onChange}/>
+            <input type="text" placeholder={placeholderText} onChange={onChange} size="1" />
         </div>
 )}
 
