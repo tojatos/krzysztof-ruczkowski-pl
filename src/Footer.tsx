a89b3c0f1b7c822383d9c547b20a81a45f633919
tojatos <tojatos@gmail.com>
2021-11-28 14:44:15 +0100

    fox favicon added; small adjustments added

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -4,8 +4,11 @@ import './Footer.sass';
 const Footer = ({myEmail} : {myEmail: string}) => {
     return (
         <footer id="main-footer">
-            <h3>Skontaktuj się ze mną</h3>
-            <p>Email: <a href={"mailto:" + myEmail}><strong>{myEmail}</strong></a></p>
+            <div className="contact">
+                <h3>Skontaktuj się ze mną</h3>
+                <p>Email: <a href={"mailto:" + myEmail}><strong>{myEmail}</strong></a></p>
+            </div>
+            <div className="copyright">Favicon made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
         </footer>
     )
 }
