dac6eda8aaa1f10625bc7b9caf308961e3cb46d4
tojatos <tojatos@gmail.com>
2022-01-09 05:00:23 +0100

    added contact icons and changed site design a bit

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -4,9 +4,10 @@ import './Footer.sass';
 const Footer = ({myEmail} : {myEmail: string}) => {
     return (
         <footer id="main-footer">
-            <div className="contact">
-                <h3>Skontaktuj się ze mną</h3>
-                <p>Email: <a href={"mailto:" + myEmail}><strong>{myEmail}</strong></a></p>
+            <div className="icons">
+                <a href={"mailto:" + myEmail}><i className="icon mail-icon"/></a>
+                <a href="https://github.com/tojatos" target="_blank" rel="noreferrer"><i className="icon github-logo"/></a>
+                <a href="https://www.linkedin.com/in/krzysztof-ruczkowski/" target="_blank" rel="noreferrer"><i className="icon linkedin-logo"/></a>
             </div>
             <div className="copyright">Favicon made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
         </footer>
